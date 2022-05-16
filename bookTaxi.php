aa6e90d40a7230d7c43f9e1014ea4142917b5bb1
Charles Monaghan <charles@charlesmonaghan.com>
2022-05-17 00:36:41 +1200

    can now post to php

---
+++ bookTaxi.php
@@ -0,0 +1,13 @@
+<?php
+    $cname = $_POST['cname'];
+    $phone = $_POST['phone'];
+    $unumber = $_POST['unumber'];
+    $snumber = $_POST['snumber'];
+    $stname = $_POST['stname'];
+    $sbname = $_POST['sbname'];
+    $dsbname = $_POST['dsbname'];
+    $date = $_POST['date'];
+    $time = $_POST['time'];
+
+    echo "<alert>test</alert>";
+?>
--- booking.js
+++ booking.js
@@ -1,3 +1,4 @@
+// get current date and time
 window.onload = function () {
   const date = new Date();
   document.getElementById("date").valueAsDate = date;
@@ -6,3 +7,29 @@ window.onload = function () {
     ":" +
     ("0" + date.getMinutes()).slice(-2);
 };
+
+function getData() {
+  const cname = document.getElementById("cname").value;
+  const phone = document.getElementById("phone").value;
+  const unumber = document.getElementById("unumber").value;
+  const snumber = document.getElementById("snumber").value;
+  const stname = document.getElementById("stname").value;
+  const sbname = document.getElementById("sbname").value;
+  const dsbname = document.getElementById("dsbname").value;
+  const date = document.getElementById("date").value;
+  const time = document.getElementById("time").value;
+
+  const xhr = createRequest();
+  if (xhr) {
+    var obj = document.getElementById("test");
+    const requestbody = `cname=${cname}&phone=${phone}&unumber=${unumber}&snumber=${snumber}&stname=${stname}&sbname=${sbname}&dsbname=${dsbname}&date=${date}&time=${time}`;
+    xhr.open("POST", "./bookTaxi.php", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        obj.innerHTML = xhr.responseText;
+      } // end if
+    }; // end anonymous call-back function
+    xhr.send(requestbody);
+  } // end if
+} // end function getData()

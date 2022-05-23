03ac0ee6bd690660f9752f957c0b1a8f74cfe922
Charles Monaghan <charles@charlesmonaghan.com>
2022-05-23 22:17:38 +1200

    can now retrieve bookings

--- admin.js
+++ admin.js
@@ -1,20 +1,24 @@
-let form = document.getElementById("form");
+let form = document.getElementById('form');
 
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   const date = new Date();
   const time =
-    ("0" + date.getHours()).slice(-2) +
-    ":" +
-    ("0" + date.getMinutes()).slice(-2);
+    ('0' + date.getHours()).slice(-2) +
+    ':' +
+    ('0' + date.getMinutes()).slice(-2);
 
   event.preventDefault();
-  const bsearch = document.getElementById("bsearch").value;
+  let bsearch = document.getElementById('bsearch').value;
+
+  if (bsearch === '') {
+    bsearch = 'empty';
+  }
 
   const xhr = createRequest();
   if (xhr) {
-    var obj = document.getElementById("response");
-    xhr.open("POST", "./getBookings.php");
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    var obj = document.getElementById('response');
+    xhr.open('POST', './getBookings.php');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     const requestbody = `bsearch=${bsearch}&date=${date.toLocaleDateString()}&time=${time}`;
     xhr.onreadystatechange = function () {
       if (xhr.readyState == 4 && xhr.status == 200) {
--- getBookings.php
+++ getBookings.php
@@ -12,34 +12,57 @@
 	$sql_db
 	);
 
-    //SELECT * FROM `booking` WHERE time >= CURRENT_TIME-2 AND date = CURRENT_DATE
-    $query = "SELECT * FROM `booking` WHERE time >= CURRENT_TIME-2 AND date = CURRENT_DATE";
-    $queryResult = $conn->query($query);
-    $output = " ";
-    if ($queryResult->num_rows> 0 ) {
-        while($row = $queryResult->fetch_assoc()) {
-            $currentStatus = "unassigned";
-            if ($row["status"] == 1) {
-                $currentStatus = "assigned";
+    if (strlen($bsearch) > 0) {
+        $query = "SELECT * FROM `booking` where ref = '$bsearch'";
+        $queryResult = $conn->query($query);
+        $output = " ";
+        if ($queryResult->num_rows> 0 ) {
+            while($row = $queryResult->fetch_assoc()) {
+                $currentStatus = "unassigned";
+                if ($row["status"] == 1) {
+                    $currentStatus = "assigned";
+                }
+                $output .= "<tr>
+                                <th scope=\"row\">".$row["ref"]."</th>
+                                <td>".$row["cname"]."</td>
+                                <td>".$row["phone"]."</td>
+                                <td>".$row["sbname"]."</td>
+                                <td>".$row["dsbname"]."</td>
+                                <td>".$row["date"].$row["time"]."</td>
+                                <td>".$currentStatus."</td>
+                                <td><button type=\"button\" class=\"btn btn-primary\">Assign</button></td>
+                            </tr>";
             }
-            $output .= "<tr>
-                            <th scope=\"row\">".$row["ref"]."</th>
-                            <td>".$row["cname"]."</td>
-                            <td>".$row["phone"]."</td>
-                            <td>".$row["sbname"]."</td>
-                            <td>".$row["dsbname"]."</td>
-                            <td>".$row["date"].$row["time"]."</td>
-                            <td>".$currentStatus."</td>
-                            <td><button type=\"button\" class=\"btn btn-primary\">Assign</button></td>
-                        </tr>";
+            echo $output;
+    } elseif ($bsearch == "empty") {
+        $query = "SELECT * FROM `booking` WHERE `time` <= CURRENT_TIME + INTERVAL 2 HOUR AND `time` >= CURRENT_TIME AND `date` = CURRENT_DATE AND `status` = 0";
+        $queryResult = $conn->query($query);
+        $output = "";
+        if ($queryResult->num_rows> 0 ) {
+            while($row = $queryResult->fetch_assoc()) {
+                $currentStatus = "unassigned";
+                if ($row["status"] == 1) {
+                    $currentStatus = "assigned";
+                }
+                $output .= "<tr>
+                                <th scope=\"row\">".$row["ref"]."</th>
+                                <td>".$row["cname"]."</td>
+                                <td>".$row["phone"]."</td>
+                                <td>".$row["sbname"]."</td>
+                                <td>".$row["dsbname"]."</td>
+                                <td>".$row["date"].$row["time"]."</td>
+                                <td>".$currentStatus."</td>
+                                <td><button type=\"button\" class=\"btn btn-primary\">Assign</button></td>
+                            </tr>";
+            }
+            if (empty($output)) {
+                echo "No results found";
+            } else {
+                echo $output;
+            }
         }
-        echo $output;
     } else {
-        echo "no results";
+        echo "Your input was incorrect";
+     }
     }
-
-    // if ($bsearch == "") {
-    //     echo "empty".$date.$time;
-    // }
-
 ?>
